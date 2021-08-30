77218b44ccddf58b6b9c6e0ee0f0e7243fce56cc
Christoph Ladurner <christoph.ladurner@tugraz.at>
2021-08-30 22:02:28 +0200

    bugfix the index.js file in the root was not compatible with nodejs
    it was not possible to use the package in a nodejs command line script, because
    of the 'export * from ./dist'. export was not recogniced. but now it is possible
    to use the package in the browser and in the nodejs scripts.

--- index.js
+++ index.js
@@ -1,10 +1,84 @@
-// for now it is necessary to have this file here. also the file paths part in
-// the otu tsconfig is necessary, it has to point to the dist directory.
-// otherwise it is not possible to import it, only javascript could be imported
-// (found no configuration to build the library ts from the consumer). only this
-// way it is possible to use the type definitions from the providing library.
-// there has to be changed something when a published package is created, maybe
-// there is some inspiration from the svelte package.
-// so if geometric is changed, rebuild or activate the watcher
-
-export * from "./dist";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.cross = exports.close = exports.Segment = exports.Vector = exports.Line = exports.Point = exports.angleToRadians = exports.angleToDegrees = exports.angleReflect = exports.isConvex = exports.isReflex = exports.angleBisector = exports.polygonIntersectsPolygon = exports.polygonInPolygon = exports.pointWithLine = exports.pointOnLine = exports.pointRightofLine = exports.pointLeftofLine = exports.pointOnPolygon = exports.pointInPolygon = exports.lineIntersectsPolygon = exports.sharePoint = exports.intersection = exports.lineIntersectsLine = exports.polygonTranslate = exports.polygonScaleY = exports.polygonScaleX = exports.polygonScale = exports.polygonRotate = exports.polygonRegular = exports.polygonMean = exports.polygonLength = exports.polygonHull = exports.polygonCentroid = exports.polygonBounds = exports.polygonArea = exports.pointTranslate = exports.pointRotate = exports.lineMidpoint = exports.distance = exports.lineLength = exports.lineInterpolate = exports.segmentAngleSegment = exports.lineAngle = void 0;
+var lineAngle_1 = require("./dist/lines/lineAngle");
+Object.defineProperty(exports, "lineAngle", { enumerable: true, get: function () { return lineAngle_1.lineAngle; } });
+Object.defineProperty(exports, "segmentAngleSegment", { enumerable: true, get: function () { return lineAngle_1.segmentAngleSegment; } });
+var lineInterpolate_1 = require("./dist/lines/lineInterpolate");
+Object.defineProperty(exports, "lineInterpolate", { enumerable: true, get: function () { return lineInterpolate_1.lineInterpolate; } });
+var lineLength_1 = require("./dist/lines/lineLength");
+Object.defineProperty(exports, "lineLength", { enumerable: true, get: function () { return lineLength_1.lineLength; } });
+Object.defineProperty(exports, "distance", { enumerable: true, get: function () { return lineLength_1.distance; } });
+var lineMidpoint_1 = require("./dist/lines/lineMidpoint");
+Object.defineProperty(exports, "lineMidpoint", { enumerable: true, get: function () { return lineMidpoint_1.lineMidpoint; } });
+var pointRotate_1 = require("./dist/points/pointRotate");
+Object.defineProperty(exports, "pointRotate", { enumerable: true, get: function () { return pointRotate_1.pointRotate; } });
+var pointTranslate_1 = require("./dist/points/pointTranslate");
+Object.defineProperty(exports, "pointTranslate", { enumerable: true, get: function () { return pointTranslate_1.pointTranslate; } });
+var polygonArea_1 = require("./dist/polygons/polygonArea");
+Object.defineProperty(exports, "polygonArea", { enumerable: true, get: function () { return polygonArea_1.polygonArea; } });
+var polygonBounds_1 = require("./dist/polygons/polygonBounds");
+Object.defineProperty(exports, "polygonBounds", { enumerable: true, get: function () { return polygonBounds_1.polygonBounds; } });
+var polygonCentroid_1 = require("./dist/polygons/polygonCentroid");
+Object.defineProperty(exports, "polygonCentroid", { enumerable: true, get: function () { return polygonCentroid_1.polygonCentroid; } });
+var polygonHull_1 = require("./dist/polygons/polygonHull");
+Object.defineProperty(exports, "polygonHull", { enumerable: true, get: function () { return polygonHull_1.polygonHull; } });
+var polygonLength_1 = require("./dist/polygons/polygonLength");
+Object.defineProperty(exports, "polygonLength", { enumerable: true, get: function () { return polygonLength_1.polygonLength; } });
+var polygonMean_1 = require("./dist/polygons/polygonMean");
+Object.defineProperty(exports, "polygonMean", { enumerable: true, get: function () { return polygonMean_1.polygonMean; } });
+var polygonRegular_1 = require("./dist/polygons/polygonRegular");
+Object.defineProperty(exports, "polygonRegular", { enumerable: true, get: function () { return polygonRegular_1.polygonRegular; } });
+var polygonRotate_1 = require("./dist/polygons/polygonRotate");
+Object.defineProperty(exports, "polygonRotate", { enumerable: true, get: function () { return polygonRotate_1.polygonRotate; } });
+var polygonScale_1 = require("./dist/polygons/polygonScale");
+Object.defineProperty(exports, "polygonScale", { enumerable: true, get: function () { return polygonScale_1.polygonScale; } });
+var polygonScaleX_1 = require("./dist/polygons/polygonScaleX");
+Object.defineProperty(exports, "polygonScaleX", { enumerable: true, get: function () { return polygonScaleX_1.polygonScaleX; } });
+var polygonScaleY_1 = require("./dist/polygons/polygonScaleY");
+Object.defineProperty(exports, "polygonScaleY", { enumerable: true, get: function () { return polygonScaleY_1.polygonScaleY; } });
+var polygonTranslate_1 = require("./dist/polygons/polygonTranslate");
+Object.defineProperty(exports, "polygonTranslate", { enumerable: true, get: function () { return polygonTranslate_1.polygonTranslate; } });
+var lineIntersectsLine_1 = require("./dist/relationships/lineIntersectsLine");
+Object.defineProperty(exports, "lineIntersectsLine", { enumerable: true, get: function () { return lineIntersectsLine_1.lineIntersectsLine; } });
+Object.defineProperty(exports, "intersection", { enumerable: true, get: function () { return lineIntersectsLine_1.intersection; } });
+var lineIntersectsLine_2 = require("./dist/relationships/lineIntersectsLine");
+Object.defineProperty(exports, "sharePoint", { enumerable: true, get: function () { return lineIntersectsLine_2.sharePoint; } });
+var lineIntersectsPolygon_1 = require("./dist/relationships/lineIntersectsPolygon");
+Object.defineProperty(exports, "lineIntersectsPolygon", { enumerable: true, get: function () { return lineIntersectsPolygon_1.lineIntersectsPolygon; } });
+var pointInPolygon_1 = require("./dist/relationships/pointInPolygon");
+Object.defineProperty(exports, "pointInPolygon", { enumerable: true, get: function () { return pointInPolygon_1.pointInPolygon; } });
+var pointOnPolygon_1 = require("./dist/relationships/pointOnPolygon");
+Object.defineProperty(exports, "pointOnPolygon", { enumerable: true, get: function () { return pointOnPolygon_1.pointOnPolygon; } });
+var pointOnLine_1 = require("./dist/relationships/pointOnLine");
+Object.defineProperty(exports, "pointLeftofLine", { enumerable: true, get: function () { return pointOnLine_1.pointLeftofLine; } });
+Object.defineProperty(exports, "pointRightofLine", { enumerable: true, get: function () { return pointOnLine_1.pointRightofLine; } });
+Object.defineProperty(exports, "pointOnLine", { enumerable: true, get: function () { return pointOnLine_1.pointOnLine; } });
+Object.defineProperty(exports, "pointWithLine", { enumerable: true, get: function () { return pointOnLine_1.pointWithLine; } });
+var polygonInPolygon_1 = require("./dist/relationships/polygonInPolygon");
+Object.defineProperty(exports, "polygonInPolygon", { enumerable: true, get: function () { return polygonInPolygon_1.polygonInPolygon; } });
+var polygonIntersectsPolygon_1 = require("./dist/relationships/polygonIntersectsPolygon");
+Object.defineProperty(exports, "polygonIntersectsPolygon", { enumerable: true, get: function () { return polygonIntersectsPolygon_1.polygonIntersectsPolygon; } });
+var angle_1 = require("./dist/relationships/angle");
+Object.defineProperty(exports, "angleBisector", { enumerable: true, get: function () { return angle_1.angleBisector; } });
+Object.defineProperty(exports, "isReflex", { enumerable: true, get: function () { return angle_1.isReflex; } });
+Object.defineProperty(exports, "isConvex", { enumerable: true, get: function () { return angle_1.isConvex; } });
+var angleReflect_1 = require("./dist/angles/angleReflect");
+Object.defineProperty(exports, "angleReflect", { enumerable: true, get: function () { return angleReflect_1.angleReflect; } });
+var angleToDegrees_1 = require("./dist/angles/angleToDegrees");
+Object.defineProperty(exports, "angleToDegrees", { enumerable: true, get: function () { return angleToDegrees_1.angleToDegrees; } });
+var angleToRadians_1 = require("./dist/angles/angleToRadians");
+Object.defineProperty(exports, "angleToRadians", { enumerable: true, get: function () { return angleToRadians_1.angleToRadians; } });
+var Point_1 = require("./dist/types/Point");
+Object.defineProperty(exports, "Point", { enumerable: true, get: function () { return Point_1.Point; } });
+var Line_1 = require("./dist/types/Line");
+Object.defineProperty(exports, "Line", { enumerable: true, get: function () { return Line_1.Line; } });
+var Vector_1 = require("./dist/types/Vector");
+Object.defineProperty(exports, "Vector", { enumerable: true, get: function () { return Vector_1.Vector; } });
+var Segment_1 = require("./dist/types/Segment");
+Object.defineProperty(exports, "Segment", { enumerable: true, get: function () { return Segment_1.Segment; } });
+var closePolygon_1 = require("./dist/utils/closePolygon");
+Object.defineProperty(exports, "close", { enumerable: true, get: function () { return closePolygon_1.close; } });
+var crossProduct_1 = require("./dist/utils/crossProduct");
+Object.defineProperty(exports, "cross", { enumerable: true, get: function () { return crossProduct_1.cross; } });
+//# sourceMappingURL=index.js.map
